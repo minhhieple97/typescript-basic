b98014bd29971c24e8eda4740a56f8a86499dbc4
HiepCongChua <hieplevuc@gmail.com>
2020-09-13 15:29:03 +0700

    nothing

--- config_setup_slow/src/index.ts
+++ config_setup_slow/src/index.ts
@@ -1,3 +1,10 @@
-const userName = "hiep";
-console.log(userName);
-export {};
+const button = document.querySelector("button");
+function add(n1: number, n2: number) {
+  if (n1 + n2 > 0) {
+    return n1 + n2;
+  }
+}
+function clickHandler(event: MouseEvent) {
+  console.log("Clicked!" + event);
+}
+button?.addEventListener("click", clickHandler);
