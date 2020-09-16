affc0870898c5d6d921e6f979837e3a40e0c0707
HiepCongChua <hieplevuc@gmail.com>
2020-09-16 09:31:16 +0700

    update

--- oop_slow/src/index.ts
+++ oop_slow/src/index.ts
@@ -1,55 +1,114 @@
-// interface Person {
-//     name: string;
-//     age: number;
-//     greet(phrase: string): void
-// }
-// let user1: Person;
-// user1 = {
-//     name: 'hiep', age: 23, greet: function (pharse: string) {
-//         console.log(pharse + '' + this.name)
-//     }
-// }
-// user1.greet('Hi there , my name is ')
-
-// So sánh interface vs type
-// Về mặt chức năng type và interface không có gì khác biệt thâm chí type sẽ đa năng hơn, type cũng có khả năng mô tả một object, type còn có thể lưu trữ những thứ như union-type và tuple type
-// Chúng ta nên sử dụng Interface khi cần mô tả một đối tượng, mục đích chuyên môn của nó là như vậy
-// Ngoài ra interface còn có thể được implement bởi class, một class có khả năng implement từ một hoặc nhiều interface
-
-
-interface Info {
-    readonly id: string,//chỉ được set duy nhất một lần
-    name: string,
-    job?: string,//bạn sử dụng dấu ? trong trường hợp không muốn bắt buộc các class được implement từ Interfa
-    gym?: (method: string) => void
+abstract class Department {
+  static fiscalYear = 2020;
+  private employees: string[] = []
+  constructor(protected readonly id: string, public name: string) {
+    // Vời từ khóa private 
+  }
+  abstract describe(this: Department): void;
+  addEmployee(employee: string) {
+    this.employees.push(employee)
+  }
+  printEmployee() {
+    console.log(this.employees.length);
+    console.log(this.employees)
+  }
+  static createEmployee(name: string) {
+    return { name }
+  }
 }
-interface Greetable extends Info {
-    greet(phrase: string): void
+const employee = Department.createEmployee('hiep')
+// const accounting = new Department("d1", "Accounting");
+// accounting.addEmployee('Hiep');
+// accounting.addEmployee('Le')
+// accounting.describe();
+// accounting.printEmployee();
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    super(id, 'IT');
+    this.admins = admins
+  }
+  describe(this: ITDepartment) {
+    console.log(`ITDepartment Department ${this.name} + id: ${this.id}`)
+  }
 }
-class Person implements Greetable {// Do Person implement từ Greetable nên nó phải tuân thủ cấu trúc của interface này, có thể bổ sung các filed.
-    public name: string
-    public job?: string
-    constructor(name: string, public id: string, job?: string) {
-        this.name = name
-        if (job) {
-            this.job = job
-        }
+const idDepartment = new ITDepartment('d2', ['hiep']);
+console.log(idDepartment)
+
+class AccountingDepartment extends Department {
+  private lastReport: string;
+  private static instance: AccountingDepartment
+  protected static numUser: number
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport
     }
-    greet(phrase: string) {
+    throw new Error('No report found.')
+  }
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please pass in a valid value!");
 
     }
-
+    this.addReport(value)
+  }
+  private constructor(id: string, private reports: string[]) {// private constructor mục đích chính của nó là class này chỉ được tạo nên duy nhất một đối tượng
+    super(id, 'accounting');
+    this.lastReport = reports[0]
+    AccountingDepartment.instance
+  }
+  static getInstance() {//từ khóa static có thể truy cập được các biến static thông qua từ khoa this do static chỉ được gọi bởi class, lúc này this sẽ trỏ vào chính class đó
+    if (AccountingDepartment.instance) {//check xem instance đã được define hay chưa thì trả về
+      return this.instance
+    }
+    this.instance = new AccountingDepartment('d1', [])
+  }
+  addReport(text: string) {
+    this.reports.push(text);
+    this.lastReport = text
+  }
+  getReports() {
+    console.log(this.reports)
+  }
+  describe() {//Overide method describe của Department
+    console.log(`Acccounting Department ${this.id} : ${this.name}`)
+  }
 }
-// So sánh một chút với abstract class thì khá giống interface nhưng cso điểm khác biệt đầu tiên đó là đối với abstract class thì mỗi class chỉ được phép extends từ duy nhất một abstract class mà thôi
-// Đối với abstract class thì mỗi abstract method sẽ được ghi đè ở class extends từ abstract class, còn ở interface thì đơn giản class implement từ interface đó nó chỉ triển khai theo đúng khuôn mẫu.
-const hiep = new Person('lehiep', '23');
-// hiep.id = 45
+// const accountingDepartment = new AccountingDepartment('d3', ['hieple']);
+// console.log(accountingDepartment)
+// accountingDepartment.mostRecentReport = ''
+// Về cơ bản từ khóa abstract sẽ buộc các class con phải triển khai một abstract method từ lớp cha, lớp cha chỉ mô tả tên hàm, kiểu giá trị trả về và tham số của hàm đó còn việc khai báo sẽ là việc của class con
+const acccounting = AccountingDepartment.getInstance()// singleton dựa trên private constructor nó có thể hữu ích trong trường hợp bạn muốn tạo object từ class nhưng muốn các đối tượng luôn giống nhau mỗi lần được tạo ra, nói ngắn gọn và đơn giản trên class singleton chúng ta chỉ có thể tạo ra duy nhất một instance từ nó. 
+console.log(acccounting)
 
-// Sử dụng Interface để thay thế Type
-type AddFnType = (n1: number, n2: number) => number;//dùng Type
-let addFnType: AddFnType
-addFnType = (n1: number, n2: number) => n1 + n2
-interface AddFnInterface {//Dùng Interface
-    (n1: number, b: number): number
+
+abstract class Warrior {
+  readonly name: string;
+  public weapon?: string;
+  constructor(name: string, weapon?: string) {
+    this.name = name;
+    this.weapon = weapon
+  }
+  sayHi(): void {
+    console.log(`Hello, I am ${this.name}`);
+  }
+
+  abstract arm(weapon: string): void; // hàm này phải được triển khai ở lớp dẫn xuất 
 }
-let addFnInterface: AddFnInterface
+
+class SuperWarrior extends Warrior {
+  constructor(name: string,) {
+    super(name); // hàm khởi tạo trong lớp dẫn xuất phải gọi super()
+  }
+  arm(weapon: string): void {
+    console.log(`${this.name} is a super warrior fighting with ${weapon}`);
+  }
+  fly(): void {
+    console.log(`${this.name} can fly`);
+  }
+}
+// let hercules: Warrior;
+// hercules = new Warrior()
+const hercules = new SuperWarrior('hiep');
+hercules.arm('guom');
+hercules.fly()
+hercules.sayHi()
---
+++ oop_slow/src/interface.ts
@@ -0,0 +1,58 @@
+// interface Person {
+//     name: string;
+//     age: number;
+//     greet(phrase: string): void
+// }
+// let user1: Person;
+// user1 = {
+//     name: 'hiep', age: 23, greet: function (pharse: string) {
+//         console.log(pharse + '' + this.name)
+//     }
+// }
+// user1.greet('Hi there , my name is ')
+
+// So sánh interface vs type
+// Về mặt chức năng type và interface không có gì khác biệt thâm chí type sẽ đa năng hơn, type cũng có khả năng mô tả một object, type còn có thể lưu trữ những thứ như union-type và tuple type
+// Chúng ta nên sử dụng Interface khi cần mô tả một đối tượng, mục đích chuyên môn của nó là như vậy
+// Ngoài ra interface còn có thể được implement bởi class, một class có khả năng implement từ một hoặc nhiều interface
+
+
+interface Info {
+    readonly id: string,//chỉ được set duy nhất một lần
+    name: string,
+    job?: string,//bạn sử dụng dấu ? trong trường hợp không muốn bắt buộc các class được implement từ Interfa
+    gym?: (method: string) => void
+}
+interface Hobbies {
+    name: string
+}
+interface Greetable extends Info, Hobbies {
+    greet(phrase: string): void
+}
+class Person implements Greetable {// Do Person implement từ Greetable nên nó phải tuân thủ cấu trúc của interface này, có thể bổ sung các filed.
+    public name: string
+    public job?: string
+    constructor(name: string, public id: string, job?: string) {
+        this.name = name
+        if (job) {
+            this.job = job
+        }
+    }
+    greet(phrase: string) {
+
+    }
+
+}
+// So sánh một chút với abstract class thì khá giống interface nhưng cso điểm khác biệt đầu tiên đó là đối với abstract class thì mỗi class chỉ được phép extends từ duy nhất một abstract class mà thôi
+// Đối với abstract class thì mỗi abstract method sẽ được ghi đè ở class extends từ abstract class, còn ở interface thì đơn giản class implement từ interface đó nó chỉ triển khai theo đúng khuôn mẫu.
+const hiep = new Person('lehiep', '23');
+// hiep.id = 45
+
+// Sử dụng Interface để thay thế Type
+type AddFnType = (n1: number, n2: number) => number;//dùng Type
+let addFnType: AddFnType
+addFnType = (n1: number, n2: number) => n1 + n2
+interface AddFnInterface {//Dùng Interface
+    (n1: number, b: number): number
+}
+let addFnInterface: AddFnInterface
