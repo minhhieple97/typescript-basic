11635fea44e24e8298e90b4909529673421d79e0
minhhieple <hieplevuc@gmail.com>
2020-12-29 17:15:50 +0700

    update

--- st_web/src/index.ts
+++ st_web/src/index.ts
@@ -1,5 +1,13 @@
 import User from "./models/User";
 
 const user = new User({ name: "hiep", age: 12 });
+user.set({ name: "Le Minh Hiep", id: '1' });
+user.on('add', () => {
+    console.log("User added")
+})
+user.trigger('add');
+user.fetch();
+
 console.log({ user });
-user.set({ name: "Le Minh Hiep" });
+
+user.save()
--- st_web/src/models/User.ts
+++ st_web/src/models/User.ts
@@ -1,17 +1,46 @@
+import axios, { AxiosResponse } from 'axios'
 interface UserProps {
+  id?: string,
   name?: string;
   age?: number;
 }
 type Cb = () => void;
 class User {
   events: { [key: string]: Cb[] } = {};
-  constructor(private data: UserProps) {}
+  constructor(private data: UserProps) { }
   get<K extends keyof UserProps>(propName: K): UserProps[K] {
     return this.data[propName];
   }
   set(update: UserProps): void {
     Object.assign(this.data, update);
   }
-  on(eventName: string, callback: Cb): void {}
+  on(eventName: string, callback: Cb): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+    if (!handlers || handlers.length === 0) return;
+    handlers.forEach(cb => {
+      cb()
+    })
+  }
+  fetch(): void {
+    axios.get(`http://localhost:3000/users/${this.get('id')}`)
+      .then((response: AxiosResponse) => {
+        console.log({ response })
+        this.set(response.data)
+      })
+  }
+  save(): void {
+    const id = this.get('id');
+    if (id) {
+      axios.put(`http://localhost:3000/users/${id}`)
+    }
+    else {
+      axios.post(`http://localhost:3000/users`, this.data)
+    }
+  }
 }
 export default User;
